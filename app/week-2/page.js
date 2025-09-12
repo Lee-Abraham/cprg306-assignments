6da7dbf381b0b759efd4d924bed61cbd05af560d
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-09-12 16:25:08 -0600

    Final

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,12 @@
+import StudentInfo from "./student-info";
+
 export default function Page() { 
     return ( 
         <main> 
-            <h1>Shopping List</h1> 
+            <h1>Shopping List</h1>
+            <div>
+                <StudentInfo></StudentInfo>
+            </div>
         </main> 
     ); 
 } 
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,21 +3,21 @@ import Link from 'next/link';
 export default function StudentInfo() { 
     return ( 
         <main> 
-            // Student Information for Lee
+            {/* Student Information for Lee */}
             <div>
                 <h2>Lee</h2>
             </div>
 
-            // Student Information for Kevin
+            {/* Student Information for Kevin */}
             <div>
                 <h2>Kevin</h2>
             </div>
 
-            // Student Information for Aileen
+             {/* Student Information for Aileen */}
             <div>
                 <h2>Aileen</h2>
             </div>
-            // Link to GitHub Repository
+            {/* Link to GitHub Repository */}
             <nav>
                 <Link href="https://github.com/Lee-Abraham/cprg306-assignments"> GitHub repository </Link>
             </nav>
