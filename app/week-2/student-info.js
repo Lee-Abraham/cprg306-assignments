4066fcd0b7942b1279bc813864258d5e9c8d4758
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-09-12 16:02:29 -0600

    Up

---
+++ app/week-2/student-info.js
@@ -0,0 +1,20 @@
+export default function StudentInfo() { 
+    return ( 
+        <main> 
+            // Student Information for Lee
+            <div>
+
+            </div>
+
+            // Student Information for Kevin
+            <div>
+
+            </div>
+
+            // Student Information for Aileen
+            <div>
+
+            </div>
+        </main> 
+    ); 
+}
