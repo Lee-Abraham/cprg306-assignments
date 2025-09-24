1e295bd59b45e6aadfeffe20a142c603baed2538
Kevin Yabut <156222818+KevinYabut08@users.noreply.github.com>
2025-09-24 09:56:50 -0600

    updated week-3 assignment

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,6 +1,13 @@
-// app/week-3/item.js
-export default function Item() {
+export default function Item({name, quantity, category}) {
+
   return (
-    <></>
+    <main>
+    <ul>
+    <li className="list-none bg-amber-800 border-2 rounded-md p-2 hover:bg-amber-600 cursor-pointer mt-3 w-100">
+      <p className="text-3xl font-bold text-start">{name} {quantity}</p>
+      <p className="text-2xl text-start">This product is in {category}</p>
+    </li>
+    </ul>
+    </main>
   );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,11 +1,11 @@
-// app/week-3/page.js
-import ItemList from "./item-list";
+import ItemList from './item-list';
+import Item from './item';
 
 export default function Page() {
   return (
     <main className="flex flex-col items-center text-center p-6">
-      <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
-      <ItemList />
+      <h1 className="text-5xl font-bold mb-4">Shopping List</h1>
+      <ItemList/>
     </main>
   );
 }
