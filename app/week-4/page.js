ac28e86eab4785362483d5e6c9c3521682936a86
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-09-26 15:14:52 -0600

    Ups

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -5,34 +5,35 @@ import {useState} from 'react';
 export default function Page() {
     const [quantity, setQuantity] = useState(1);
 
+    function incrementQuantity() {
+        if (quantity <= 20) {
+            setQuantity(quantity + 1);
+        }
+        else {
+            alert("Quantity cannot be more than 20");
+        }
+    }
+
+    function decrementQuantity() {
+        if (quantity >= 1) {
+            setQuantity(quantity - 1);
+        }
+        else {
+            alert("Quantity cannot be less than 1");
+        }
+    }
+
   return (
     <main className="flex flex-col items-center text-center p-6">
         <NewItem />
         <div style={{display: "flex", alignItems: "center", gap: "10px"}}>
             <span className="mx-2">{quantity}</span>
             <div>
-                <button style={{borderRadius: "10px", backgroundColor: "green"}} onClick={incrementQuantity}>Increase</button>
-                <button style={{borderRadius: "10px", backgroundColor: "red"}} onClick={decrementQuantity}>Decrease</button>
+                <button style={{borderRadius: "10px", backgroundColor: "green", margin: "10px"}} onClick={incrementQuantity}>Increase</button>
+                <button style={{borderRadius: "10px", backgroundColor: "red", margin: "10px"}} onClick={decrementQuantity}>Decrease</button>
             </div>
         </div>
     </main>
   );
 }
 
-function incrementQuantity() {
-    if (quantity <= 20) {
-        setQuantity(quantity + 1);
-    }
-    else {
-        alert("Quantity cannot be more than 20");
-    }
-}
-
-function decrementQuantity() {
-    if (quantity >= 1) {
-        setQuantity(quantity - 1);
-    }
-    else {
-        alert("Quantity cannot be less than 1");
-    }
-}
