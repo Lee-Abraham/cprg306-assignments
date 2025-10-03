43db384c046e87f03cad0a289d67e97cf5866e6e
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-10-02 22:47:41 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    // Offloads Webpack to a separate thread to reduce memory usage
     webpackBuildWorker: true
-  },
-  // Optional: disable Turbopack in production if it's causing issues
-  turbo: {
-    enabled: false
   }
 };
 
