bc41511fdd89d639c896a4db704590fa092e4312
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-10-03 15:25:27 -0600

    Update new-item.js

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -2,8 +2,10 @@
 import {useState} from 'react';
 
 export default function NewItem() {
+    // State to hold the quantity value
       const [quantity, setQuantity] = useState(1);
 
+    // Function to handle incrementing the quantity
     function incrementQuantity() {
         if (quantity < 20) {
             setQuantity(quantity + 1);
@@ -13,6 +15,7 @@ export default function NewItem() {
         }
     }
 
+    // Function to handle decrementing the quantity
     function decrementQuantity() {
         if (quantity > 1) {
             setQuantity(quantity - 1);
@@ -22,10 +25,11 @@ export default function NewItem() {
         }
     }
 
+// Render the component
   return (
     <main className="flex items-center text-center p-6">
         <div className="flex items-center gap-2 bg-white rounded-[10px] p-4 shadow-lg mt-6 size-[10px,10px]">
-            <span className="mx-2 text-5xl font-bold mb-4 text-black rounded-[10p]">{quantity}</span>
+            <p className="mx-2 text-5xl font-bold mb-4 text-black rounded-[10p]">{quantity}</p>
             <div className="flex gap-2 px-4">
                 <button className=" text-xl font-bold mb-4 rounded-[10px] bg-green-600 text-black px-4 py-2 transition-colors duration-200 hover:bg-[#006400]" onClick={incrementQuantity}>
                     +
