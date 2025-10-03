223140182373ee07918f46e901f1c2ce09e70e71
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-10-03 15:19:56 -0600

    Update new-item.js

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -23,9 +23,9 @@ export default function NewItem() {
     }
 
   return (
-    <main className="flex items-center text-center p-6 size-2">
-        <div className="flex items-center gap-2 bg-white rounded-[10px] p-4 shadow-lg mt-6">
-            <span className="mx-2 text-5xl font-bold mb-4 text-black">{quantity}</span>
+    <main className="flex items-center text-center p-6">
+        <div className="flex items-center gap-2 bg-white rounded-[10px] p-4 shadow-lg mt-6 size-[10px,10px]">
+            <span className="mx-2 text-5xl font-bold mb-4 text-black rounded-[10p]">{quantity}</span>
             <div className="flex gap-2 px-4">
                 <button className=" text-xl font-bold mb-4 rounded-[10px] bg-green-600 text-black px-4 py-2 transition-colors duration-200 hover:bg-[#006400]" onClick={incrementQuantity}>
                     +
