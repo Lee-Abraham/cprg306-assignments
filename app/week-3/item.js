305bd98c44c082611e60e2ff50428d68efdc9530
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-10-03 15:13:04 -0600

    up

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,13 +1,14 @@
 export default function Item({item}) {
 const {name, quantity, category } = item;
+
   return (
     <main>
-    <ul>
-    <li className="list-none bg-amber-800 border-2 rounded-md p-2 hover:bg-amber-600 cursor-pointer mt-3 w-100">
-      <p className="text-3xl font-bold text-start">{name} {quantity}</p>
-      <p className="text-2xl text-start">This product is in {category}</p>
-    </li>
-    </ul>
+      <ul>
+        <li className="list-none bg-amber-800 border-2 rounded-md p-2 hover:bg-amber-600 cursor-pointer mt-3 w-100">
+          <p className="text-3xl font-bold text-start">{name} {quantity}</p>
+          <p className="text-2xl text-start">This product is in {category}</p>
+        </li>
+      </ul>
     </main>
   );
 }
