0013edeec10e5bac22e6a67bc72750e35b2bfca7
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-10-10 14:04:34 -0600

    Up

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -25,10 +25,10 @@ export default function NewItem() {
         <div className="flex items-center gap-2 bg-white rounded-[10px] p-4 shadow-lg mt-6 size-[10px,10px] border-2 border-black">
             <p className="mx-2 text-5xl font-bold mb-4 text-black rounded-[10p]">{quantity}</p>
             <div className="flex gap-2 px-4">
-                <button className=" text-xl font-bold mb-4 rounded-[10px] bg-green-600 text-black px-4 py-2 transition-colors duration-200 hover:bg-[#006400]" onClick={incrementQuantity} disabled={quantity = 20}>
+                <button className=" text-xl font-bold mb-4 rounded-[10px] bg-green-600 text-black px-4 py-2 transition-colors duration-200 hover:bg-[#006400]" onClick={incrementQuantity} disabled={quantity == 20}>
                     +
                 </button>
-                <button className="text-xl font-bold mb-4 rounded-[10px] bg-red-600 text-black px-4 py-2 transition-colors duration-200 hover:bg-[#660000]" onClick={decrementQuantity} disabled={quantity = 1 }>
+                <button className="text-xl font-bold mb-4 rounded-[10px] bg-red-600 text-black px-4 py-2 transition-colors duration-200 hover:bg-[#660000]" onClick={decrementQuantity} disabled={quantity == 1 }>
                     -
                 </button>
             </div>
